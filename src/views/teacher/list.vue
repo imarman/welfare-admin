18ca860c258d182db55fe082fb7e68f06d20ead0
Arman <life.is2short@outlook.com>
2021-12-04 21:48:18 +0800

    配置信息

---
+++ src/views/teacher/list.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-container">
+    hello teacher list
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TeacherList'
+}
+</script>
+
+<style scoped>
+
+</style>
