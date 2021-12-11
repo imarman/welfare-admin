2e3fd7f78e10a4e521361f5a3aff3f3039140556
Arman <life.is2short@outlook.com>
2021-12-11 18:05:39 +0800

    用户修改密码，并且修改成功重新登录

--- src/utils/request.js
+++ src/utils/request.js
@@ -50,10 +50,10 @@ service.interceptors.response.use(
     // if the custom code is not 20000, it is judged as an error.
     if (res.code !== '99999') {
       // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-      if (res.code === '30000' || res.code === '30002' || res.code === '28004') {
+      if (res.code === '30000' || res.code === '28004') {
         // to re-login
         MessageBox.confirm(
-          '登录状态已过期，请重新重新登录',
+          '登录状态已过期，请重新登录',
           '系统提示',
           {
             confirmButtonText: '重新登录',
@@ -64,11 +64,15 @@ service.interceptors.response.use(
             location.reload()
           })
         })
+      } else if (res.code === '30002') {
+        Message.error(res.message)
+        return res
       } else if (res.code === '28005') {
         router.replace({ path: '/401' })
         return res
       } else if (res.code === '28000') {
         Message.error(res.message)
+        return res
       }
       Message.error(res.message)
       return Promise.reject(new Error(res.message || 'Error'))
--- src/views/user/pwd.vue
+++ src/views/user/pwd.vue
@@ -1,14 +1,87 @@
 <template>
-  <div class="app-container">
-    修改密码
+  <div class="app-container u-center">
+    <el-form :model="dialogForm">
+      <el-form-item label="用户名" label-width="120px">
+        <el-input v-model="username" disabled style="width: 300px" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="旧密码" label-width="120px">
+        <el-input v-model="dialogForm.oldPassword" style="width: 300px" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="新密码" label-width="120px">
+        <el-input v-model="dialogForm.newPassword" type="password" style="width: 300px" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="重复新密码" label-width="120px">
+        <el-input v-model="repeatPwd" style="width: 300px" type="password" autocomplete="off" />
+      </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button type="primary" @click="submit">修 改</el-button>
+    </span>
   </div>
 </template>
 
 <script>
+import { getUserInfo, changePwd } from '@/api/userCenter'
+import store from '@/store'
+
 export default {
+  data() {
+    return {
+      id: this.$store.state.user.id,
+      dialogForm: {},
+      username: '',
+      repeatPwd: ''
+    }
+  },
+  created() {
+    this.userInfo()
+  },
+  methods: {
+    async submit() {
+      if (this.repeatPwd !== this.dialogForm.newPassword) {
+        this.$message.error('两次密码不一样')
+        this.dialogForm.newPassword = ''
+        this.repeatPwd = ''
+        return
+      }
+      this.dialogForm.id = this.id
+      console.log(this.dialogForm)
+      const res = await changePwd(this.dialogForm)
+      if (res.success) {
+        try {
+          await this.$confirm(
+            '密码已修改成功，请重新登录',
+            '系统提示',
+            {
+              confirmButtonText: '重新登录',
+              cancelButtonText: '取消',
+              type: 'warning'
+            })
+          store.dispatch('user/resetToken').then(() => {
+            location.reload()
+          })
+        } catch (e) {
+          this.$message.error(e)
+        }
+      }
+    },
+    async userInfo() {
+      const res = await getUserInfo(this.id)
+      this.username = res.data.username
+    }
+  }
 }
 </script>
 
 <style scoped>
+.u-center {
+  margin: 50px 600px;
+}
+.dialog-footer  {
+  margin-left: 345px;
+}
 
+el-form-item {
+  margin-right: 20px;
+}
 </style>
